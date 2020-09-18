f24cffd0c00cadc335272f9c2ca32957798bbf5a
Minh <squirrelwhisky@gmail.com>
2020-09-19 02:05:07 +0700

    finishing content

--- script.js
+++ script.js
@@ -1,46 +1,100 @@
 const CHOICE = ["Rock", "Paper", "Scissors"];
 function computerPlay() {
-return CHOICE[Math.floor(Math.random() * CHOICE.length)];
-}
-
-String.prototype.standardize = function () {
-if (this.length > 1)
-    return this.substr(0, 1).toUpperCase() + this.substr(1, this.length - 1).toLowerCase(); 
+    return CHOICE[Math.floor(Math.random() * CHOICE.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-let state = "";
-let winner = "";
-let loser = "";
-playerSelection = playerSelection.standardize();
-if (   playerSelection == "Paper" && computerSelection == "Rock"
-    || playerSelection == "Rock" && computerSelection == "Scissors"
-    || playerSelection == "Scissors" && computerSelection == "Paper") {
-    state = "Win";
-    winner = playerSelection;
-    loser = computerSelection;
-}
-else if (   playerSelection == "Paper" && computerSelection == "Scissors"
-        ||  playerSelection == "Rock" && computerSelection == "Paper"
-        ||  playerSelection == "Scissors" && computerSelection == "Rock") {
-    state = "Lose";
-    winner = computerSelection;
-    loser = playerSelection;
-}
-else {
-    state = "Draw";
-}
-return state == "Draw" ? "Draw!" : `You ${state}! ${winner} beats ${loser}`;
+    if (   playerSelection == "Paper" && computerSelection == "Rock"
+        || playerSelection == "Rock" && computerSelection == "Scissors"
+        || playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "Win";
+    }
+    else if (   playerSelection == "Paper" && computerSelection == "Scissors"
+            ||  playerSelection == "Rock" && computerSelection == "Paper"
+            ||  playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "Lose";
+    }
+    else {
+        return "Draw";
+    }
 }
 
-function game() {
-let computerSelection = "";
-for (let _ = 0; _ < 5; _++) {
-    let playerSelection = "";
-    while (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
-    playerSelection = prompt("Enter your choice: ").standardize();
+function resetAll() {
+    setTimeout(() => {
+        final.style.display = "none";
+        score = 0;
+        playerChoice = "";
+        computerChoice = "";
+        scoreHTML.innerHTML = 0;
+        playerShow.innerHTML = `<img class="flyingfromleft" src="300_8.png">`;
+        computerShow.innerHTML = `<img class="flyingfromright" src="300_1.png">`;
+        if (!choicesHTML.classList.contains("highlight"))
+            choicesHTML.classList.add("highlight");
+    }, 5000);
+}
+
+function toggleClass(e) {
+    e.classList.toggle("congrats");
+}
+
+function handler(e) {
+    let playerChoice = e.target.getAttribute("data-choice");
+    let computerChoice = computerPlay();
+    setTimeout(() => choicesHTML.classList.remove("highlight"), 100);
+    playerShow.innerHTML = `<img class="flyingfromleft" src="${playerChoice}.png">`;
+    computerShow.innerHTML = `<img class="flyingfromright" src="${computerChoice}.png">`;
+    let state = playRound(playerChoice, computerChoice);
+    if (state == "Win") {
+        toggleClass(playerName);
+        scoreHTML.innerHTML = ++score;
+        setTimeout(() => toggleClass(playerName), 700);
     }
-    computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+    else if (state == "Draw") {
+        toggleClass(playerName);
+        setTimeout(() => toggleClass(playerName), 700);
+        toggleClass(computerName);
+        setTimeout(() => toggleClass(computerName), 700);
+    }
+    else {
+        toggleClass(computerName);
+        scoreHTML.innerHTML = --score;
+        setTimeout(() => toggleClass(computerName), 700);
+    }
+    if (score == 5) {
+        final.style.display = "flex";
+        resetAll();
+        scoreHTML.innerHTML = score;
+    }
+    if (score != 5)
+        setTimeout(() => choicesHTML.classList.add("highlight"), 2000);
 }
-}
+
+
+const scoreHTML = document.querySelector(".scoreChange");
+const choicesHTML = document.querySelector(".choices");
+const dataChoices = Array.from(document.querySelectorAll(".choices [data-choice]"));
+const playerName = document.querySelector(".playerName");
+const computerName = document.querySelector(".computerName");
+const playerShow = document.querySelector(".playerChoice");
+const computerShow = document.querySelector(".computerChoice");
+const final = document.querySelector(".final");
+const button = document.querySelector("button");
+
+let score = 0
+let playerChoice = "";
+let computerChoice = "";
+
+button.addEventListener("click",  () => setTimeout(() => {
+    final.style.display = "none";
+    score = 0;
+    playerChoice = "";
+    computerChoice = "";
+    scoreHTML.innerHTML = 0;
+    playerShow.innerHTML = `<img class="flyingfromleft" src="300_8.png">`;
+    computerShow.innerHTML = `<img class="flyingfromright" src="300_1.png">`;
+    choicesHTML.classList.add("highlight");;
+}, 500));
+
+choicesHTML.classList.add("highlight");
+for (let _ = 0; _ < dataChoices.length; _++)
+    dataChoices[_].addEventListener("click", handler);
